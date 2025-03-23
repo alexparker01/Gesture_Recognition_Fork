e9e7dc767cc11130515c253451fb9aec963acd91
Alex Parker <alexparker43@gmail.com>
2025-03-23 00:20:27 +0000

    Added BLE functionality following the web article (i think it works but not sure and cant test without the Arduino

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +34,52 @@ class GestureControlledOutputApp extends StatefulWidget {
 class _GestureControlledOutputAppState extends State<GestureControlledOutputApp> {
   int _inputValue = 0; //This is the int that will be fed by the Arduino based on gesture
   final _audioPlayer = AudioPlayer(); //This is the audio player for the alert sounds
+  final _ble = FlutterReactiveBle(); //This is the BLE connection to the Arduino
+
+  StreamSubscription<DiscoveredDevice>? _scanSub;
+  StreamSubscription<ConnectionStateUpdate>? _connectSub;
+  StreamSubscription<List<int>>? _notifySub;
+
+  var _found = false;
+  var _value = '';
+
+  @override
+  initState() {
+    super.initState();
+    _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
+  }
+
+  @override
+  dispose() {
+    _scanSub?.cancel();
+    _connectSub?.cancel();
+    _notifySub?.cancel();
+    super.dispose();
+  }
+
+  void _onScanUpdate(DiscoveredDevice d) {
+    if (d.name == 'BLE-TEMP' && !_found) {  // NEED TO RENAME THIS TO THE NAME OF THE ARDUINO
+      _found = true;
+      _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
+        if (update.connectionState == DeviceConnectionState.connected) {
+          _onConnected(d.id);
+        }
+      });
+    }
+  }
+
+  void _onConnected(String deviceId) {
+    final characteristic = QualifiedCharacteristic(
+        deviceId: deviceId,
+        serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
+        characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'));
+
+    _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
+      setState(() {
+        _value = const Utf8Decoder().convert(bytes);
+      });
+    });
+  }
 
   Color _getBackgroundColor() { //This sets the bg colour based on the input value
     if (_inputValue == 0) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import audioplayers_darwin
 import path_provider_foundation
+import reactive_ble_mobile
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ReactiveBlePlugin.register(with: registry.registrar(forPlugin: "ReactiveBlePlugin"))
 }
