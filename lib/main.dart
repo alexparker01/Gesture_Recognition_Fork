189687d3e957457482d4bac102a4d11f1282f72e
Alex Parker <alexparker43@gmail.com>
2025-03-24 16:44:33 +0000

    Changed to accept String data and uses that for the gesture displayed

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class GestureControlledOutputApp extends StatefulWidget {
 }
 
 class _GestureControlledOutputAppState extends State<GestureControlledOutputApp> {
-  int _inputValue = 0; //This is the int that will be fed by the Arduino based on gesture
+  var _inputVar = "empty"; //This is the variable that will be fed by the Arduino based on gesture
   final _audioPlayer = AudioPlayer(); //This is the audio player for the alert sounds
   final _ble = FlutterReactiveBle(); //This is the BLE connection to the Arduino
 
@@ -41,7 +41,6 @@ class _GestureControlledOutputAppState extends State<GestureControlledOutputApp>
   StreamSubscription<List<int>>? _notifySub;
 
   var _found = false;
-  var _value = '';
 
   @override
   initState() {
@@ -58,11 +57,12 @@ class _GestureControlledOutputAppState extends State<GestureControlledOutputApp>
   }
 
   void _onScanUpdate(DiscoveredDevice d) {
-    if (d.name == 'BLE-TEMP' && !_found) {  // NEED TO RENAME THIS TO THE NAME OF THE ARDUINO
+    if (d.name == 'Nano33BLE' && !_found) {  // NEED TO RENAME THIS TO THE NAME OF THE ARDUINO
       _found = true;
       _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
         if (update.connectionState == DeviceConnectionState.connected) {
           _onConnected(d.id);
+          print("CONNECTED TO A DEVICE");
         }
       });
     }
@@ -71,26 +71,28 @@ class _GestureControlledOutputAppState extends State<GestureControlledOutputApp>
   void _onConnected(String deviceId) {
     final characteristic = QualifiedCharacteristic(
         deviceId: deviceId,
-        serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
-        characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'));
+        serviceId: Uuid.parse('19B10000-E8F2-537E-4F6C-D104768A1214'),
+        characteristicId: Uuid.parse('19B10001-E8F2-537E-4F6C-D104768A1214'));
 
     _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
       setState(() {
-        _value = const Utf8Decoder().convert(bytes);
+        String gesture = const Utf8Decoder().convert(bytes).trim();
+        print("Received gesture: $gesture");
+        _readGesture(gesture); // Process the incoming gesture
       });
     });
   }
 
   Color _getBackgroundColor() { //This sets the bg colour based on the input value
-    if (_inputValue == 0) {
+    if (_inputVar == "empty") {
       return Colors.grey;
-    } else if (_inputValue == 1) {
+    } else if (_inputVar == "figure8") {
       return Colors.blue;
-    } else if (_inputValue == 2) {
+    } else if (_inputVar == "vertical") {
       return Colors.green;
-    } else if (_inputValue == 3) {
+    } else if (_inputVar == "clockwise") {
       return Colors.pink.shade200;
-    } else if (_inputValue == 4) {
+    } else if (_inputVar == "horizontal") {
       return Colors.red;
     } else {
       return Colors.yellow.shade200;
@@ -98,41 +100,39 @@ class _GestureControlledOutputAppState extends State<GestureControlledOutputApp>
   }
 
   String _getDisplayText() { //This sets the text based on the input value
-    if (_inputValue == 0) {
+    if (_inputVar == "empty") {
       return "Awaiting input gesture";
-    } else if (_inputValue == 1) {
+    } else if (_inputVar == "figure8") {
       return "Patient is thirsty";
-    } else if (_inputValue == 2) {
+    } else if (_inputVar == "vertical") {
       return "Patient is hungry";
-    } else if (_inputValue == 3) {
+    } else if (_inputVar == "clockwise") {
       return "Patient needs the restroom";
-    } else if (_inputValue == 4) {
+    } else if (_inputVar == "horizontal") {
       return "Patient is in pain";
     } else {
       return "Gesture not recognised";
     }
   }
 
-  Future<void> updateSound(int value) async { //This plays the calmer alert for non-emergencies and the urgent one for emergencies
+  Future<void> updateSound(String gesture) async { //This plays the calmer alert for non-emergencies and the urgent one for emergencies
     await _audioPlayer.stop();
-    if (value == 0) {
+    if (gesture == "empty") {
       // pass (isn't a thing in dart)
-    } else if (value < 4) {
-      await _audioPlayer.setReleaseMode(ReleaseMode.loop);
-      await _audioPlayer.play(AssetSource('sounds/calm.mp3'));
-    } else if (value == 4) {
+    } else if (gesture == "horizontal") {
       await _audioPlayer.setReleaseMode(ReleaseMode.loop);
       await _audioPlayer.play(AssetSource('sounds/urgent.mp3'));
     } else {
-      // pass
+      await _audioPlayer.setReleaseMode(ReleaseMode.loop);
+      await _audioPlayer.play(AssetSource('sounds/calm.mp3'));
     }
   }
 
-  void _readGesture(int value) { //This updates the currently read gesture. Controlled by buttons for now, but will be replaced with the Arduino input
+  void _readGesture(String gesture) { //This updates the currently read gesture. Controlled by buttons for now, but will be replaced with the Arduino input
     setState(() {
-      _inputValue = value;
+      _inputVar = gesture;
     });
-    updateSound(value);
+    updateSound(gesture);
   }
 
   @override
@@ -149,29 +149,25 @@ class _GestureControlledOutputAppState extends State<GestureControlledOutputApp>
             ),
             const SizedBox(height: 50),
             ElevatedButton( //This button is for the carer to clear the alert once they respond to it
-              onPressed: () => _readGesture(0),
+              onPressed: () => _readGesture("empty"),
               child: const Text('Clear Gesture'),
             ),
             const SizedBox(height: 50),
             ElevatedButton( //This button and all the following ones, will be removed when this app is connected to the Arduino input
-              onPressed: () => _readGesture(1),
+              onPressed: () => _readGesture("figure8"),
               child: const Text('Thirsty'),
             ),
             ElevatedButton(
-              onPressed: () => _readGesture(2),
+              onPressed: () => _readGesture("vertical"),
               child: const Text('Hungry'),
             ),
             ElevatedButton(
-              onPressed: () => _readGesture(3),
+              onPressed: () => _readGesture("clockwise"),
               child: const Text('Restroom'),
             ),
             ElevatedButton(
-              onPressed: () => _readGesture(4),
+              onPressed: () => _readGesture("horizontal"),
               child: const Text('Pain'),
-            ),
-            ElevatedButton(
-              onPressed: () => _readGesture(5),
-              child: const Text('Unrecognised gesture'),
             ),
           ],
         ),
